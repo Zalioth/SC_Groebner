66dd58842d284a6ebd1acb328cbbf0d3dad562a9
José Ignacio Carmona Villegas <Zalioth@gmail.com>
2014-01-16 13:20:51 +0100

    All tests ready to be executed. For cleaner results, wipe the contents of files test2.txt, test3.txt and test4.txt, but it's not critical, since at the beginning of the test, the full configuration is printed to the file.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 import util.Time;
-import edu.jas.arith.BigComplex;
 import edu.jas.arith.ModInteger;
 import edu.jas.arith.ModIntegerRing;
 import edu.jas.gb.GroebnerBase;
@@ -24,16 +22,20 @@ public class Main
 		Time timer = new Time();
 		
 		int minVars = 1;
-		int maxVars = 5;
+		int maxVars = 7;
 		int minPoly = 1;
-		int maxPoly = 5;
+		int maxPoly = 7;
 		int minDegr = 1;
-		int maxDegr = 5;
+		int maxDegr = 7;
 		
-		int fixed = 3;
 		int increment = 1;
 		
-		int repetitions = 5;
+		int minFixed = 1;
+		int maxFixed = 6;
+		
+		int incrementFixed = 2;
+		
+		int repetitions = 10;
 		
 		/*
 		// Test 1, full cubic test.
@@ -61,122 +63,195 @@ public class Main
 		System.out.println("\tTest 1 ended.\n");
 		*/
 		
-		// Test 2, cuadratic test vars-poly.
+		// Test 2, quadratic test vars-poly.
 		System.out.println("\tTest 2 starting...\n");
 		FileWriter fw2 = new FileWriter("./test2.txt", true);
       PrintWriter pw2 = new PrintWriter(fw2);
       
-      pw2.print( "v-p" );
-      for(int p=minPoly; p<maxPoly; p+=increment)
-		{
-      	pw2.print( "\t" + p );
-		}
-      pw2.print("\n");
-		for(int v=minVars; v<maxVars; v+=increment)
-		{
-			pw2.print(v);
-			for(int p=minPoly; p<maxPoly; p+=increment)
-			{
-				int d = fixed;
-				long sum = 0;
-				for(int r=0; r<repetitions; ++r)
-				{
-					// Execute the method #calculateGroebnerBase and measure it's time
-					timer.start();
-					calculateGroebnerBase(v, p, d);
-					timer.stop();
-					
-					sum += timer.getTime();
-				}
-				
-				long average = sum / repetitions;
-				
-				// Write the time in a file
-				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
-				pw2.print( ( "\t" + average ) );
-			}
-			pw2.print("\n");
-		}
+      pw2.println("");
+      pw2.println("Test 2");
+      pw2.println("Quadratic test, Vars - Poly");
+      pw2.println("minVars: " + minVars + " maxVars: " + maxVars);
+      pw2.println("minPoly: " + minPoly + " maxPoly: " + maxPoly);
+      pw2.println("minDegr: " + minDegr + " maxDegr: " + maxDegr);
+      pw2.println("With an increment of " + increment);
+      pw2.println("And a time average calculated from " + repetitions + " repetitions");
+      pw2.println("Fixed value varies as:");
+      pw2.println("minFixed: " + minFixed + " maxFixed: " + maxFixed);
+      pw2.println("With an increment of " + incrementFixed);
+      pw2.println("");
+      
+      for(int f=minFixed; f<maxFixed; f+=incrementFixed)
+      {
+      	pw2.println("fixed: " + f);
+      	pw2.print( "v-p" );
+         for(int p=minPoly; p<maxPoly; p+=increment)
+   		{
+         	pw2.print( "\t" + p );
+   		}
+         pw2.print("\n");
+   		for(int v=minVars; v<maxVars; v+=increment)
+   		{
+   			pw2.print(v);
+   			for(int p=minPoly; p<maxPoly; p+=increment)
+   			{
+   				int d = f;
+   				long sum = 0;
+   				for(int r=0; r<repetitions; ++r)
+   				{
+   					// Execute the method #calculateGroebnerBase and measure it's time
+   					timer.start();
+   					calculateGroebnerBase(v, p, d);
+   					timer.stop();
+   					
+   					sum += timer.getTime();
+   				}
+   				
+   				long average = sum / repetitions;
+   				
+   				// Write the time in a file
+   				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
+   				pw2.print( ( "\t" + average ) );
+   			}
+   			pw2.print("\n");
+   		}
+   		pw2.println("");
+      }
+		
 		pw2.close();
 		fw2.close();
 		System.out.println("\tTest 2 ended.\n");
 		
-		// Test 3, cuadratic test vars-degr.
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// Test 3, quadratic test vars-degr.
 		System.out.println("\tTest 3 starting...\n");
 		FileWriter fw3 = new FileWriter("./test3.txt", true);
       PrintWriter pw3 = new PrintWriter(fw3);
       
-      pw3.print( "v-d" );
-      for(int d=minDegr; d<maxDegr; d+=increment)
-		{
-      	pw3.print( "\t" + d );
-		}
-      pw3.print("\n");
-		for(int v=minVars; v<maxVars; v+=increment)
-		{
-			pw3.print(v);
-			for(int d=minDegr; d<maxDegr; d+=increment)
-			{
-				int p = fixed;
-				long sum = 0;
-				for(int r=0; r<repetitions; ++r)
-				{
-					// Execute the method #calculateGroebnerBase and measure it's time
-					timer.start();
-					calculateGroebnerBase(v, p, d);
-					timer.stop();
-					
-					sum += timer.getTime();
-				}
-				
-				long average = sum / repetitions;
-				
-				// Write the time in a file
-				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
-				pw3.print( ( "\t" + average ) );
-			}
-			pw3.print("\n");
-		}
+      pw3.println("");
+      pw3.println("Test 3");
+      pw3.println("Quadratic test, Vars - Degr");
+      pw3.println("minVars: " + minVars + " maxVars: " + maxVars);
+      pw3.println("minPoly: " + minPoly + " maxPoly: " + maxPoly);
+      pw3.println("minDegr: " + minDegr + " maxDegr: " + maxDegr);
+      pw3.println("With an increment of " + increment);
+      pw3.println("And a time average calculated from " + repetitions + " repetitions");
+      pw3.println("Fixed value varies as:");
+      pw3.println("minFixed: " + minFixed + " maxFixed: " + maxFixed);
+      pw3.println("With an increment of " + incrementFixed);
+      pw3.println("");
+      
+      for(int f=minFixed; f<maxFixed; f+=incrementFixed)
+      {
+      	pw3.println("fixed: " + f);
+      	pw3.print( "v-d" );
+         for(int d=minDegr; d<maxDegr; d+=increment)
+   		{
+         	pw3.print( "\t" + d );
+   		}
+         pw3.print("\n");
+   		for(int v=minVars; v<maxVars; v+=increment)
+   		{
+   			pw3.print(v);
+   			for(int d=minDegr; d<maxDegr; d+=increment)
+   			{
+   				int p = f;
+   				long sum = 0;
+   				for(int r=0; r<repetitions; ++r)
+   				{
+   					// Execute the method #calculateGroebnerBase and measure it's time
+   					timer.start();
+   					calculateGroebnerBase(v, p, d);
+   					timer.stop();
+   					
+   					sum += timer.getTime();
+   				}
+   				
+   				long average = sum / repetitions;
+   				
+   				// Write the time in a file
+   				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
+   				pw3.print( ( "\t" + average ) );
+   			}
+   			pw3.print("\n");
+   		}
+      	pw3.println("");
+      }
+      
 		pw3.close();
 		fw3.close();
 		System.out.println("\tTest 3 ended.\n");
 		
-		// Test 4, cuadratic test poly-degr.
+		
+		
+		
+		
+		
+		
+		
+		// Test 4, quadratic test poly-degr.
 		System.out.println("\tTest 4 starting...\n");
 		FileWriter fw4 = new FileWriter("./test4.txt", true);
       PrintWriter pw4 = new PrintWriter(fw4);
       
-      pw4.print( "p-d" );
-      for(int d=minDegr; d<maxDegr; d+=increment)
-		{
-      	pw4.print( "\t" + d );
-		}
-      pw4.print("\n");
-		for(int p=minPoly; p<maxPoly; p+=increment)
-		{
-			pw4.print(p);
-			for(int d=minDegr; d<maxDegr; d+=increment)
-			{
-				int v = fixed;
-				long sum = 0;
-				for(int r=0; r<repetitions; ++r)
-				{
-					// Execute the method #calculateGroebnerBase and measure it's time
-					timer.start();
-					calculateGroebnerBase(v, p, d);
-					timer.stop();
-					
-					sum += timer.getTime();
-				}
-				
-				long average = sum / repetitions;
-				
-				// Write the time in a file
-				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
-				pw4.print( ( "\t" + average ) );
-			}
-			pw4.print("\n");
-		}
+      pw4.println("");
+      pw4.println("Test 4");
+      pw4.println("Quadratic test, Poly - Degr");
+      pw4.println("minVars: " + minVars + " maxVars: " + maxVars);
+      pw4.println("minPoly: " + minPoly + " maxPoly: " + maxPoly);
+      pw4.println("minDegr: " + minDegr + " maxDegr: " + maxDegr);
+      pw4.println("With an increment of " + increment);
+      pw4.println("And a time average calculated from " + repetitions + " repetitions");
+      pw4.println("Fixed value varies as:");
+      pw4.println("minFixed: " + minFixed + " maxFixed: " + maxFixed);
+      pw4.println("With an increment of " + incrementFixed);
+      pw4.println("");
+      
+      for(int f=minFixed; f<maxFixed; f+=incrementFixed)
+      {
+      	pw4.println("fixed: " + f);
+      	pw4.print( "p-d" );
+         for(int d=minDegr; d<maxDegr; d+=increment)
+   		{
+         	pw4.print( "\t" + d );
+   		}
+         pw4.print("\n");
+   		for(int p=minPoly; p<maxPoly; p+=increment)
+   		{
+   			pw4.print(p);
+   			for(int d=minDegr; d<maxDegr; d+=increment)
+   			{
+   				int v = f;
+   				long sum = 0;
+   				for(int r=0; r<repetitions; ++r)
+   				{
+   					// Execute the method #calculateGroebnerBase and measure it's time
+   					timer.start();
+   					calculateGroebnerBase(v, p, d);
+   					timer.stop();
+   					
+   					sum += timer.getTime();
+   				}
+   				
+   				long average = sum / repetitions;
+   				
+   				// Write the time in a file
+   				System.out.println(("v: " + v + "\tp: " + p + "\td: " + d + "\ttime: " + average ));
+   				pw4.print( ( "\t" + average ) );
+   			}
+   			pw4.print("\n");
+   		}
+      	pw4.println("");
+      }
+      
 		pw4.close();
 		fw4.close();
 		System.out.println("\tTest 4 ended.\n");
